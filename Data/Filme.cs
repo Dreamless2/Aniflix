31592d39eac28f8969ff997546dc39e5c7f8a64f
Tiago Pereira <tiagonet50@gmail.com>
2024-12-29 17:15:06 -0300

    created filme model

---
+++ Data/Filme.cs
@@ -0,0 +1,17 @@
+﻿namespace Aniflix.Data
+{
+    public class Filme
+    {
+        public string? Codigo { get; set; }
+        public string? Titulo { get; set; }
+        public string? TituloOriginal { get; set; }
+        public string? DataLancamento { get; set; }
+        public string? Franquia { get; set; }
+        public string? Genero { get; set; }
+        public string? Tags { get; set; }
+        public string? Diretor { get; set; }
+        public string? Estrelas { get; set; }
+        public string? Estudio { get; set; }
+        public string? Sinopse { get; set; }
+    }
+}
--- Models/AniflixDbContext.cs
+++ Models/AniflixDbContext.cs
@@ -7,18 +7,5 @@ namespace Aniflix.Models
         public DbSet<Filme> Filmes { get; set; }
     }
 
-    public class Filme
-    {
-        public string? Codigo { get; set; }
-        public string? Titulo { get; set; }
-        public string? TituloOriginal { get; set; }
-        public string? DataLancamento { get; set; }
-        public string? Franquia { get; set; }
-        public string? Genero { get; set; }
-        public string? Tags { get; set; }
-        public string? Diretor { get; set; }
-        public string? Estrelas { get; set; }
-        public string? Estudio { get; set; }
-        public string? Sinopse { get; set; }
-    }
+
 }
