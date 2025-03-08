00e63fecdc876c91dcdb56dd4eb16f7ba2f68359
Dreamless2 <tiagonet50@gmail.com>
2025-03-07 23:51:02 -0300

    Aniflix

---
+++ Views/FilmesView.cs
@@ -0,0 +1,21 @@
+﻿using Sunny.UI;
+using Aniflix.Functions;
+using FontAwesome.Sharp.Material;
+
+namespace Aniflix.Views
+{
+    public partial class FilmesView : UIForm
+    {
+        public FilmesView()
+        {
+            InitializeComponent();
+        }
+
+        private void FilmesView_Load(object sender, EventArgs e)
+        {
+            GlobalFunctions.ApplyTheme(this);
+        }
+
+
+    }
+}
