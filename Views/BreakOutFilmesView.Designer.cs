5de56c7250872ab9cb26a0034027bb5fd8cf1bbd
Dreamless2 <tiagonet50@gmail.com>
2025-03-08 00:08:03 -0300

    Update SeriesView.Designer.cs SeriesView.resx

--- Views/BreakOutFilmesView.Designer.cs
+++ Views/BreakOutFilmesView.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "BreakOutFilmesView";
+            SuspendLayout();
+            // 
+            // BreakOutFilmesView
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            ClientSize = new Size(800, 450);
+            Name = "BreakOutFilmesView";
+            Text = "BreakOut - Filmes";
+            ZoomScaleRect = new Rectangle(15, 15, 800, 450);
+            ResumeLayout(false);
         }
 
         #endregion
