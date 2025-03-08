29fda4cab7ff99318827fb93fee446988a71100a
Dreamless2 <tiagonet50@gmail.com>
2025-03-08 00:03:40 -0300

    Update AnimesFilmesView.Designer.cs AnimesFilmesView.cs AnimesFilmesView.resx

--- Views/AnimesFilmesView.cs
+++ Views/AnimesFilmesView.cs
@@ -1,16 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+using Sunny.UI;
 using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
 using System.Windows.Forms;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Aniflix.Views
 {
-    public partial class AnimesFilmesView: Form
+    public partial class AnimesFilmesView: UIForm
     {
         public AnimesFilmesView()
         {
