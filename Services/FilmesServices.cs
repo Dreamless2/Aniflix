c18f70376498131a2cd1743642b8aa0a8f2a1653
Dreamless2 <tiagonet50@gmail.com>
2025-03-08 09:12:33 -0300

    Update FilmesServices.cs

--- Services/FilmesServices.cs
+++ Services/FilmesServices.cs
@@ -12,7 +12,7 @@ namespace Aniflix.Services
         private static readonly HttpClient _httpClient = new()
         {
             BaseAddress = new Uri("https://api.themoviedb.org/3/"),
-            DefaultRequestHeaders = { { "Authorization", "Bearer SEU_TOKEN_AQUI" } }
+            DefaultRequestHeaders = { { "Authorization", "Bearer"  }
         };
 
         public async Task<T?> GetFromTmdbAsync<T>(string endpoint)
