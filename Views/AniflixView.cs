3a9d8511b3499598143fb4012db541fe5a4da718
Dreamless2 <tiagonet50@gmail.com>
2025-03-09 14:24:15 -0300

    Update AniflixView.cs

--- Views/AniflixView.cs
+++ Views/AniflixView.cs
@@ -79,7 +79,7 @@ namespace Aniflix.Views
                 Color.FromArgb(255, 62, 115),   // #FF3E73 - Rose
                 Color.FromArgb(255, 78, 69),    // #FF4E45 - Branco perfeito
                 Color.FromArgb(56, 147, 114),   // #388F72 - Esmeralda
-                Color.FromArgb(255, 87, 34),    // #FF5722 - Laranja avermelhado
+                Color.FromArgb(231, 92, 180),  // #E75CB4 - Rose
             ];
 
             int bgIndex = random.Next(darkBackgrounds.Count);
