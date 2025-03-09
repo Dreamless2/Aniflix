1ea52ae465c597c2f9d3d8d14af4f7f030f571f6
Dreamless2 <tiagonet50@gmail.com>
2025-03-09 17:06:31 -0300

    Update AniflixView.cs

--- Views/AniflixView.cs
+++ Views/AniflixView.cs
@@ -102,7 +102,7 @@ namespace Aniflix.Views
             GlobalVars.currentChildForm = childForm;
             childForm.TopLevel = false;
             childForm.Dock = DockStyle.Fill;
-            childForm.TitleColor = Color.FromArgb(109, 122, 121); // #6D7A79 - Cinza escuro
+            childForm.TitleColor = Color.FromArgb(66, 67, 72);  // #424348
             childForm.TitleForeColor = Color.Gainsboro;
             childForm.ShowRect = false;
             childForm.BringToFront();
