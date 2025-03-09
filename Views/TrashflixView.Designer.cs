d103b168a490b3901101fe19d58a604a6b8aa4cb
Dreamless2 <tiagonet50@gmail.com>
2025-03-08 23:40:54 -0300

    Update SeriesView.Designer.cs

--- Views/TrashflixView.Designer.cs
+++ Views/TrashflixView.Designer.cs
@@ -33,7 +33,7 @@
             // TrashflixView
             // 
             AutoScaleMode = AutoScaleMode.None;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1284, 859);
             Name = "TrashflixView";
             Text = "Trashflix";
             ZoomScaleRect = new Rectangle(15, 15, 800, 450);
