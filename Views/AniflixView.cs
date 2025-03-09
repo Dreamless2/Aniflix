c614c75912dfc58a14131556e1f13cb6bf099e35
Dreamless2 <tiagonet50@gmail.com>
2025-03-09 10:39:26 -0300

    Update AniflixView.cs

--- Views/AniflixView.cs
+++ Views/AniflixView.cs
@@ -75,7 +75,7 @@ namespace Aniflix.Views
                 Color.FromArgb(240, 115, 55),   // #F07337 - Siena queimado
                 Color.FromArgb(1, 180, 155),    // #01B49B - Keppel
                 Color.FromArgb(243, 124, 52),   // #F37C34 - Cadmio
-                Color.FromArgb(0, 156, 82),     // #009C52 - Shamrock green
+                Color.FromArgb(57, 97, 83),     // #396553 - Jumbo
                 Color.FromArgb(105, 160, 108),  // #69A06C - Oxley
                 Color.FromArgb(56, 147, 114),   // #388F72 - Esmeralda
             ];
