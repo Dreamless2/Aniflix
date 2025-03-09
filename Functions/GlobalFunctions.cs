7ded4884230f3fe44bfe9386bd9f75d562700bfb
Dreamless2 <tiagonet50@gmail.com>
2025-03-09 09:50:08 -0300

    Update GlobalFunctions.cs

--- Functions/GlobalFunctions.cs
+++ Functions/GlobalFunctions.cs
@@ -11,7 +11,7 @@ namespace Aniflix.Functions
             {
                 if (btns is MaterialButton btn)
                 {
-                    btn.BackColor = ThemeColor.PrimaryColor;
+                    btn.BackColor = Color.OrangeRed;
                     btn.ForeColor = Color.Gainsboro;
                     btn.FlatStyle = FlatStyle.Flat;
                     btn.FlatAppearance.BorderSize = 0;
