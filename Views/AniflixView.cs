5020c88dcc2f10a03fd6799862b1de5fa5d32b9c
Dreamless2 <tiagonet50@gmail.com>
2025-03-09 13:15:14 -0300

    Update AniflixView.cs

--- Views/AniflixView.cs
+++ Views/AniflixView.cs
@@ -59,7 +59,7 @@ namespace Aniflix.Views
             List<Color> accentColors =
             [
                 Color.FromArgb(246, 114, 75),   // #F6724B - Laranja mandarin
-                Color.FromArgb(225, 149, 71),   // #E19547 - Laranja jasper
+                //Color.FromArgb(225, 149, 71),   // #E19547 - Laranja jasper
                 Color.FromArgb(212, 163, 107),  // #D4A36B - Laranja tangerina crayon
                 Color.FromArgb(255, 152, 0),    // #FF9800 - Laranja vibrante
                 Color.FromArgb(247, 199, 103),  // #F7C767 - Laranja crayon
