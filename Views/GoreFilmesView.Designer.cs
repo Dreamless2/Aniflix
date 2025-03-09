19804be59810e4a32e064bae498edd068cb50bea
Dreamless2 <tiagonet50@gmail.com>
2025-03-08 23:40:10 -0300

    Update BreakOutFilmesView.Designer.cs

--- Views/GoreFilmesView.Designer.cs
+++ Views/GoreFilmesView.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "GoreFilmesView";
+            SuspendLayout();
+            // 
+            // GoreFilmesView
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            ClientSize = new Size(1292, 887);
+            Name = "GoreFilmesView";
+            Text = "GoreFilmesView";
+            ZoomScaleRect = new Rectangle(15, 15, 800, 450);
+            ResumeLayout(false);
         }
 
         #endregion
