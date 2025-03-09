db22a32dc4f1cfc73023631320fcb7110febab54
Dreamless2 <tiagonet50@gmail.com>
2025-03-09 19:55:01 -0300

    Update SobreView.Designer.cs

--- Views/SobreView.Designer.cs
+++ Views/SobreView.Designer.cs
@@ -30,8 +30,8 @@
         {
             uiPanel1 = new Sunny.UI.UIPanel();
             uiPanel2 = new Sunny.UI.UIPanel();
-            uiSymbolLabel1 = new Sunny.UI.UISymbolLabel();
             svgPictureBox1 = new SharpVectors.Renderers.Forms.SvgPictureBox();
+            uiSymbolLabel1 = new Sunny.UI.UISymbolLabel();
             uiPanel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)svgPictureBox1).BeginInit();
             SuspendLayout();
@@ -64,24 +64,22 @@
             uiPanel2.Text = null;
             uiPanel2.TextAlignment = ContentAlignment.MiddleCenter;
             // 
-            // uiSymbolLabel1
-            // 
-            uiSymbolLabel1.Font = new Font("Microsoft Sans Serif", 12F);
-            uiSymbolLabel1.Location = new Point(178, 258);
-            uiSymbolLabel1.MinimumSize = new Size(1, 1);
-            uiSymbolLabel1.Name = "uiSymbolLabel1";
-            uiSymbolLabel1.Size = new Size(1155, 35);
-            uiSymbolLabel1.TabIndex = 2;
-            uiSymbolLabel1.Text = "This product uses the TMDB API but is not endorsed or certified by TMDB.";
-            // 
             // svgPictureBox1
             // 
             svgPictureBox1.Location = new Point(370, 356);
             svgPictureBox1.Name = "svgPictureBox1";
             svgPictureBox1.Size = new Size(200, 200);
-            svgPictureBox1.Source = null;
             svgPictureBox1.TabIndex = 3;
-            svgPictureBox1.XmlSource = null;
+            // 
+            // uiSymbolLabel1
+            // 
+            uiSymbolLabel1.Font = new Font("Microsoft Sans Serif", 12F);
+            uiSymbolLabel1.Location = new Point(310, 291);
+            uiSymbolLabel1.MinimumSize = new Size(1, 1);
+            uiSymbolLabel1.Name = "uiSymbolLabel1";
+            uiSymbolLabel1.Size = new Size(559, 28);
+            uiSymbolLabel1.TabIndex = 2;
+            uiSymbolLabel1.Text = "This product uses the TMDB API but is not endorsed or certified by TMDB.";
             // 
             // SobreView
             // 
@@ -95,6 +93,7 @@
             Text = "SobreView";
             ZoomScaleRect = new Rectangle(15, 15, 800, 450);
             uiPanel2.ResumeLayout(false);
+            uiPanel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)svgPictureBox1).EndInit();
             ResumeLayout(false);
         }
