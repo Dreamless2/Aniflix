684b500a32f45ea4f0603af9e42322bd4dbd1d3b
Dreamless2 <tiagonet50@gmail.com>
2025-03-09 19:55:25 -0300

    Update SobreView.Designer.cs

--- Views/SobreView.Designer.cs
+++ Views/SobreView.Designer.cs
@@ -28,81 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            uiPanel1 = new Sunny.UI.UIPanel();
-            uiPanel2 = new Sunny.UI.UIPanel();
-            svgPictureBox1 = new SharpVectors.Renderers.Forms.SvgPictureBox();
-            uiSymbolLabel1 = new Sunny.UI.UISymbolLabel();
-            uiPanel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)svgPictureBox1).BeginInit();
             SuspendLayout();
-            // 
-            // uiPanel1
-            // 
-            uiPanel1.Dock = DockStyle.Top;
-            uiPanel1.Font = new Font("Microsoft Sans Serif", 12F);
-            uiPanel1.Location = new Point(0, 35);
-            uiPanel1.Margin = new Padding(4, 5, 4, 5);
-            uiPanel1.MinimumSize = new Size(1, 1);
-            uiPanel1.Name = "uiPanel1";
-            uiPanel1.Size = new Size(1598, 80);
-            uiPanel1.TabIndex = 0;
-            uiPanel1.Text = null;
-            uiPanel1.TextAlignment = ContentAlignment.MiddleCenter;
-            // 
-            // uiPanel2
-            // 
-            uiPanel2.Controls.Add(svgPictureBox1);
-            uiPanel2.Controls.Add(uiSymbolLabel1);
-            uiPanel2.Dock = DockStyle.Fill;
-            uiPanel2.Font = new Font("Microsoft Sans Serif", 12F);
-            uiPanel2.Location = new Point(0, 115);
-            uiPanel2.Margin = new Padding(4, 5, 4, 5);
-            uiPanel2.MinimumSize = new Size(1, 1);
-            uiPanel2.Name = "uiPanel2";
-            uiPanel2.Size = new Size(1598, 713);
-            uiPanel2.TabIndex = 1;
-            uiPanel2.Text = null;
-            uiPanel2.TextAlignment = ContentAlignment.MiddleCenter;
-            // 
-            // svgPictureBox1
-            // 
-            svgPictureBox1.Location = new Point(370, 356);
-            svgPictureBox1.Name = "svgPictureBox1";
-            svgPictureBox1.Size = new Size(200, 200);
-            svgPictureBox1.TabIndex = 3;
-            // 
-            // uiSymbolLabel1
-            // 
-            uiSymbolLabel1.Font = new Font("Microsoft Sans Serif", 12F);
-            uiSymbolLabel1.Location = new Point(310, 291);
-            uiSymbolLabel1.MinimumSize = new Size(1, 1);
-            uiSymbolLabel1.Name = "uiSymbolLabel1";
-            uiSymbolLabel1.Size = new Size(559, 28);
-            uiSymbolLabel1.TabIndex = 2;
-            uiSymbolLabel1.Text = "This product uses the TMDB API but is not endorsed or certified by TMDB.";
             // 
             // SobreView
             // 
             AutoScaleMode = AutoScaleMode.None;
             ClientSize = new Size(1598, 828);
-            Controls.Add(uiPanel2);
-            Controls.Add(uiPanel1);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "SobreView";
             Text = "SobreView";
             ZoomScaleRect = new Rectangle(15, 15, 800, 450);
-            uiPanel2.ResumeLayout(false);
-            uiPanel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)svgPictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
-
-        private Sunny.UI.UIPanel uiPanel1;
-        private Sunny.UI.UIPanel uiPanel2;
-        private Sunny.UI.UISymbolLabel uiSymbolLabel1;
-        private SharpVectors.Renderers.Forms.SvgPictureBox svgPictureBox1;
     }
 }
