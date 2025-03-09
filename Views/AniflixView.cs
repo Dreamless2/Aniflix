6b71eae7bdae73bf0253e62829be6e5f7cbe06d7
Dreamless2 <tiagonet50@gmail.com>
2025-03-09 09:29:31 -0300

    Update FilmesView.Designer.cs

--- Views/AniflixView.cs
+++ Views/AniflixView.cs
@@ -148,6 +148,7 @@ namespace Aniflix.Views
             currentChildForm?.Close();
             currentChildForm = childForm;
             childForm.TopLevel = false;
+            childForm.
             childForm.Dock = DockStyle.Fill;
             DesktopPanel.Controls.Add(childForm);
             DesktopPanel.Tag = childForm;
