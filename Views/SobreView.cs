d6ff5d1844909b228d4465886bdec7b3e417b6d0
Dreamless2 <tiagonet50@gmail.com>
2025-03-09 22:37:48 -0300

    Update SobreView.Designer.cs SobreView.cs

--- Views/SobreView.cs
+++ Views/SobreView.cs
@@ -26,7 +26,6 @@ namespace Aniflix.Views
 
         private void SobreView_Load(object sender, EventArgs e)
         {
-            GlobalFunctions.LoadSvgFromUrlAsync("https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg", WebCanvas);
         }
 
         private void LogoBox_Click(object sender, EventArgs e)
