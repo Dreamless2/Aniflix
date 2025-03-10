ce1819a22e2d0fb88f24096ec2dca27b5273b947
Dreamless2 <tiagonet50@gmail.com>
2025-03-09 22:38:07 -0300

    Update SobreView.cs

--- Views/SobreView.cs
+++ Views/SobreView.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Drawing;
 using Aniflix.Functions;
+using System.Diagnostics;
 using System.Windows.Forms;
 using System.ComponentModel;
 using System.Threading.Tasks;
@@ -30,7 +31,7 @@ namespace Aniflix.Views
 
         private void LogoBox_Click(object sender, EventArgs e)
         {
-
+            Process.Start("https://www.themoviedb.org/");
         }
     }
 }
