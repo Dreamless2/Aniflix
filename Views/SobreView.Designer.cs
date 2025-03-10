3609a29c31de91c0afee97b4e4c1eb1ab301e528
Dreamless2 <tiagonet50@gmail.com>
2025-03-10 19:28:58 -0300

    Update SobreView.Designer.cs

--- Views/SobreView.Designer.cs
+++ Views/SobreView.Designer.cs
@@ -143,10 +143,9 @@
             // svgPictureBox1
             // 
             svgPictureBox1.AppTitle = "Aniflix";
-            svgPictureBox1.Dock = DockStyle.Left;
             svgPictureBox1.Location = new Point(0, 0);
             svgPictureBox1.Name = "svgPictureBox1";
-            svgPictureBox1.Size = new Size(216, 230);
+            svgPictureBox1.Size = new Size(216, 106);
             svgPictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             svgPictureBox1.TabIndex = 2;
             svgPictureBox1.XmlSource = resources.GetString("svgPictureBox1.XmlSource");
