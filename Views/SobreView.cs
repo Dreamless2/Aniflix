2b48a33428630b020b661857f9e891b998e132d8
Dreamless2 <tiagonet50@gmail.com>
2025-03-09 22:52:41 -0300

    Update SobreView.cs

--- Views/SobreView.cs
+++ Views/SobreView.cs
@@ -28,6 +28,7 @@ namespace Aniflix.Views
         private void LinkSite_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             var linkData = (string)e.Link!.LinkData!;
+
             if (linkData != null)
             {
                 Process.Start(linkData);
