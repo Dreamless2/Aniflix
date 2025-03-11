eae109919c59b87752a22af7d49032c01204e75c
Dreamless2 <tiagonet50@gmail.com>
2025-03-10 21:28:06 -0300

    Update SobreView.Designer.cs

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace Aniflix.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap aniflix {
+            get {
+                object obj = ResourceManager.GetObject("aniflix", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap tmdb_logo {
             get {
                 object obj = ResourceManager.GetObject("tmdb_logo", resourceCulture);
