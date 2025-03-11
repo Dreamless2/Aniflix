5c9ddaf2f114f8dc432380b4b93e91b7647e34b0
Dreamless2 <tiagonet50@gmail.com>
2025-03-10 22:56:04 -0300

    Update SobreView.Designer.cs

--- Views/SobreView.Designer.cs
+++ Views/SobreView.Designer.cs
@@ -188,9 +188,8 @@
             LogoBox.Name = "LogoBox";
             LogoBox.Size = new Size(259, 83);
             LogoBox.SizeMode = PictureBoxSizeMode.StretchImage;
-            LogoBox.Source = null;
+            LogoBox.Source = "https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg";
             LogoBox.TabIndex = 23;
-            LogoBox.XmlSource = null;
             // 
             // pictureBox2
             // 
