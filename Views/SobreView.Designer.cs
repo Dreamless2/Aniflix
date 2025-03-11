67949cbcb53ea524f3b6cb8ac6dc719a4e0d27a7
Dreamless2 <tiagonet50@gmail.com>
2025-03-10 21:33:56 -0300

    Update SobreView.Designer.cs

--- Views/SobreView.Designer.cs
+++ Views/SobreView.Designer.cs
@@ -28,116 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            uiSymbolLabel1 = new Sunny.UI.UISymbolLabel();
-            LogoBox = new PictureBox();
-            LinkSite = new LinkLabel();
-            LicenseBox = new PictureBox();
-            LicenseSite = new LinkLabel();
-            uiSymbolLabel2 = new Sunny.UI.UISymbolLabel();
-            uiPanel1 = new Sunny.UI.UIPanel();
             uiPanel2 = new Sunny.UI.UIPanel();
+            pictureBox1 = new PictureBox();
             uiLabel1 = new Sunny.UI.UILabel();
             uiLine2 = new Sunny.UI.UILine();
             uiLine1 = new Sunny.UI.UILine();
-            pictureBox1 = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)LogoBox).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)LicenseBox).BeginInit();
-            uiPanel1.SuspendLayout();
+            uiSymbolLabel2 = new Sunny.UI.UISymbolLabel();
+            LicenseBox = new PictureBox();
+            LicenseSite = new LinkLabel();
+            LogoBox = new PictureBox();
+            LinkSite = new LinkLabel();
+            uiSymbolLabel1 = new Sunny.UI.UISymbolLabel();
             uiPanel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)LicenseBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)LogoBox).BeginInit();
             SuspendLayout();
-            // 
-            // uiSymbolLabel1
-            // 
-            uiSymbolLabel1.BackColor = Color.Transparent;
-            uiSymbolLabel1.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            uiSymbolLabel1.Location = new Point(21, 137);
-            uiSymbolLabel1.MinimumSize = new Size(1, 1);
-            uiSymbolLabel1.Name = "uiSymbolLabel1";
-            uiSymbolLabel1.Size = new Size(524, 28);
-            uiSymbolLabel1.TabIndex = 1;
-            uiSymbolLabel1.Text = "This product uses the TMDB API but is not endorsed or certified by TMDB.";
-            // 
-            // LogoBox
-            // 
-            LogoBox.Image = Properties.Resources.tmdb_logo;
-            LogoBox.Location = new Point(21, 12);
-            LogoBox.Name = "LogoBox";
-            LogoBox.Size = new Size(155, 83);
-            LogoBox.SizeMode = PictureBoxSizeMode.StretchImage;
-            LogoBox.TabIndex = 3;
-            LogoBox.TabStop = false;
-            LogoBox.Click += LogoBox_Click;
-            LogoBox.MouseEnter += LogoBox_MouseEnter;
-            // 
-            // LinkSite
-            // 
-            LinkSite.AutoSize = true;
-            LinkSite.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            LinkSite.Location = new Point(21, 108);
-            LinkSite.Name = "LinkSite";
-            LinkSite.Size = new Size(122, 16);
-            LinkSite.TabIndex = 4;
-            LinkSite.TabStop = true;
-            LinkSite.Text = "TMDB Home Page";
-            LinkSite.LinkClicked += LinkSite_LinkClicked;
-            // 
-            // LicenseBox
-            // 
-            LicenseBox.ImageLocation = "https://www.gnu.org/graphics/gplv3-with-text-136x68.png";
-            LicenseBox.Location = new Point(674, 22);
-            LicenseBox.Name = "LicenseBox";
-            LicenseBox.Size = new Size(158, 83);
-            LicenseBox.SizeMode = PictureBoxSizeMode.CenterImage;
-            LicenseBox.TabIndex = 7;
-            LicenseBox.TabStop = false;
-            LicenseBox.Click += LicenseBox_Click;
-            LicenseBox.MouseEnter += LicenseBox_MouseEnter;
-            // 
-            // LicenseSite
-            // 
-            LicenseSite.AutoSize = true;
-            LicenseSite.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            LicenseSite.Location = new Point(676, 108);
-            LicenseSite.Name = "LicenseSite";
-            LicenseSite.Size = new Size(156, 16);
-            LicenseSite.TabIndex = 8;
-            LicenseSite.TabStop = true;
-            LicenseSite.Text = "GNU GPLv3 Home Page";
-            LicenseSite.LinkClicked += LicenseSite_LinkClicked;
-            // 
-            // uiSymbolLabel2
-            // 
-            uiSymbolLabel2.BackColor = Color.Transparent;
-            uiSymbolLabel2.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            uiSymbolLabel2.Location = new Point(551, 137);
-            uiSymbolLabel2.MinimumSize = new Size(1, 1);
-            uiSymbolLabel2.Name = "uiSymbolLabel2";
-            uiSymbolLabel2.Size = new Size(281, 28);
-            uiSymbolLabel2.TabIndex = 9;
-            uiSymbolLabel2.Text = "Licenciado sob os termos da GPLv3.";
-            // 
-            // uiPanel1
-            // 
-            uiPanel1.BackColor = Color.FromArgb(237, 239, 240);
-            uiPanel1.Controls.Add(uiSymbolLabel2);
-            uiPanel1.Controls.Add(LicenseBox);
-            uiPanel1.Controls.Add(LicenseSite);
-            uiPanel1.Controls.Add(LogoBox);
-            uiPanel1.Controls.Add(LinkSite);
-            uiPanel1.Controls.Add(uiSymbolLabel1);
-            uiPanel1.Dock = DockStyle.Bottom;
-            uiPanel1.FillColor = Color.FromArgb(237, 239, 240);
-            uiPanel1.Font = new Font("Microsoft Sans Serif", 12F);
-            uiPanel1.Location = new Point(0, 264);
-            uiPanel1.Margin = new Padding(4, 5, 4, 5);
-            uiPanel1.MinimumSize = new Size(1, 1);
-            uiPanel1.Name = "uiPanel1";
-            uiPanel1.RectSides = ToolStripStatusLabelBorderSides.None;
-            uiPanel1.Size = new Size(852, 189);
-            uiPanel1.TabIndex = 10;
-            uiPanel1.Text = null;
-            uiPanel1.TextAlignment = ContentAlignment.MiddleCenter;
             // 
             // uiPanel2
             // 
@@ -156,6 +62,17 @@
             uiPanel2.TabIndex = 11;
             uiPanel2.Text = null;
             uiPanel2.TextAlignment = ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Left;
+            pictureBox1.Image = Properties.Resources.aniflix;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(240, 230);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
             // 
             // uiLabel1
             // 
@@ -194,51 +111,107 @@
             uiLine1.Size = new Size(596, 29);
             uiLine1.TabIndex = 3;
             // 
-            // pictureBox1
+            // uiSymbolLabel2
             // 
-            pictureBox1.Dock = DockStyle.Left;
-            pictureBox1.Image = Properties.Resources.aniflix;
-            pictureBox1.Location = new Point(0, 0);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(240, 230);
-            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox1.TabIndex = 6;
-            pictureBox1.TabStop = false;
+            uiSymbolLabel2.BackColor = Color.Transparent;
+            uiSymbolLabel2.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            uiSymbolLabel2.Location = new Point(548, 378);
+            uiSymbolLabel2.MinimumSize = new Size(1, 1);
+            uiSymbolLabel2.Name = "uiSymbolLabel2";
+            uiSymbolLabel2.Size = new Size(281, 28);
+            uiSymbolLabel2.TabIndex = 17;
+            uiSymbolLabel2.Text = "Licenciado sob os termos da GPLv3.";
+            // 
+            // LicenseBox
+            // 
+            LicenseBox.ImageLocation = "https://www.gnu.org/graphics/gplv3-with-text-136x68.png";
+            LicenseBox.Location = new Point(671, 276);
+            LicenseBox.Name = "LicenseBox";
+            LicenseBox.Size = new Size(158, 83);
+            LicenseBox.SizeMode = PictureBoxSizeMode.CenterImage;
+            LicenseBox.TabIndex = 15;
+            LicenseBox.TabStop = false;
+            // 
+            // LicenseSite
+            // 
+            LicenseSite.AutoSize = true;
+            LicenseSite.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            LicenseSite.Location = new Point(673, 349);
+            LicenseSite.Name = "LicenseSite";
+            LicenseSite.Size = new Size(156, 16);
+            LicenseSite.TabIndex = 16;
+            LicenseSite.TabStop = true;
+            LicenseSite.Text = "GNU GPLv3 Home Page";
+            // 
+            // LogoBox
+            // 
+            LogoBox.Image = Properties.Resources.tmdb_logo;
+            LogoBox.Location = new Point(18, 276);
+            LogoBox.Name = "LogoBox";
+            LogoBox.Size = new Size(155, 83);
+            LogoBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            LogoBox.TabIndex = 13;
+            LogoBox.TabStop = false;
+            // 
+            // LinkSite
+            // 
+            LinkSite.AutoSize = true;
+            LinkSite.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            LinkSite.Location = new Point(18, 349);
+            LinkSite.Name = "LinkSite";
+            LinkSite.Size = new Size(122, 16);
+            LinkSite.TabIndex = 14;
+            LinkSite.TabStop = true;
+            LinkSite.Text = "TMDB Home Page";
+            // 
+            // uiSymbolLabel1
+            // 
+            uiSymbolLabel1.BackColor = Color.Transparent;
+            uiSymbolLabel1.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            uiSymbolLabel1.Location = new Point(18, 378);
+            uiSymbolLabel1.MinimumSize = new Size(1, 1);
+            uiSymbolLabel1.Name = "uiSymbolLabel1";
+            uiSymbolLabel1.Size = new Size(524, 28);
+            uiSymbolLabel1.TabIndex = 12;
+            uiSymbolLabel1.Text = "This product uses the TMDB API but is not endorsed or certified by TMDB.";
             // 
             // SobreView
             // 
             AutoScaleMode = AutoScaleMode.None;
             ClientSize = new Size(852, 453);
+            Controls.Add(uiSymbolLabel2);
+            Controls.Add(LicenseBox);
+            Controls.Add(LicenseSite);
+            Controls.Add(LogoBox);
+            Controls.Add(LinkSite);
+            Controls.Add(uiSymbolLabel1);
             Controls.Add(uiPanel2);
-            Controls.Add(uiPanel1);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "SobreView";
             Text = "Sobre";
             ZoomScaleRect = new Rectangle(15, 15, 800, 450);
             Load += SobreView_Load;
-            ((System.ComponentModel.ISupportInitialize)LogoBox).EndInit();
-            ((System.ComponentModel.ISupportInitialize)LicenseBox).EndInit();
-            uiPanel1.ResumeLayout(false);
-            uiPanel1.PerformLayout();
             uiPanel2.ResumeLayout(false);
             uiPanel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)LicenseBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)LogoBox).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
-        private Sunny.UI.UISymbolLabel uiSymbolLabel1;
-        private PictureBox LogoBox;
-        private LinkLabel LinkSite;
-        private PictureBox LicenseBox;
-        private LinkLabel LicenseSite;
-        private Sunny.UI.UISymbolLabel uiSymbolLabel2;
-        private Sunny.UI.UIPanel uiPanel1;
         private Sunny.UI.UIPanel uiPanel2;
         private Sunny.UI.UILine uiLine1;
         private Sunny.UI.UILine uiLine2;
         private Sunny.UI.UILabel uiLabel1;
         private PictureBox pictureBox1;
+        private Sunny.UI.UISymbolLabel uiSymbolLabel2;
+        private PictureBox LicenseBox;
+        private LinkLabel LicenseSite;
+        private PictureBox LogoBox;
+        private LinkLabel LinkSite;
+        private Sunny.UI.UISymbolLabel uiSymbolLabel1;
     }
 }
