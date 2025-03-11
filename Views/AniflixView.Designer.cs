d3245c03bd5919715119f8fb8ca58bef28245aed
Dreamless2 <tiagonet50@gmail.com>
2025-03-11 12:35:35 -0300

    Update AniflixView.Designer.cs

--- Views/AniflixView.Designer.cs
+++ Views/AniflixView.Designer.cs
@@ -418,6 +418,7 @@
             pictureBox1.Location = new Point(0, 0);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(258, 90);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
