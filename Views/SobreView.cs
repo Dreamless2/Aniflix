83142706d5442b9dc58b56d6e59505ee664ede50
Dreamless2 <tiagonet50@gmail.com>
2025-03-10 22:53:31 -0300

    Update SobreView.Designer.cs

--- Views/SobreView.cs
+++ Views/SobreView.cs
@@ -13,6 +13,9 @@ namespace Aniflix.Views
         {
             LinkSite.Links.Add(0, LinkSite.Text.Length, "https://www.themoviedb.org/");
             LicenseSite.Links.Add(0, LicenseSite.Text.Length, "https://www.gnu.org/licenses/gpl-3.0.html");
+            LinkTMDBLib.Links.Add(0, LinkTMDBLib.Text.Length, "");
+            LinkSunnyUI.Links.Add(0, LinkSunnyUI.Text.Length, "https://github.com/SunnyUI/SunnyUI");
+            LogoBox.Source = "https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg";
         }
 
         private void LogoBox_Click(object sender, EventArgs e)
