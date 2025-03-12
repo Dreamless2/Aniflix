2224c7dbb6d875dec092a1d0b08dbf59c01ca14f
Dreamless2 <tiagonet50@gmail.com>
2025-03-12 17:43:22 -0300

    Update FilmesModel.cs

--- Models/FilmesModel.cs
+++ Models/FilmesModel.cs
@@ -9,17 +9,17 @@ namespace Aniflix.Models
     public class FilmesModel
     {
         public int Id { get; set; }
-        public string Titulo { get; set; }
-        public string Sinopse { get; set; }
-        public string TituloOriginal { get; set; }
-        public string DataLancamento { get; set; }
-        public string Tags { get; set; }
-        public string Genero { get; set; }
-        public string Diretor { get; set; }
+        public string? Titulo { get; set; }
+        public string? Sinopse { get; set; }
+        public string? TituloOriginal { get; set; }
+        public string? DataLancamento { get; set; }
+        public string? Tags { get; set; }
+        public string? Genero { get; set; }
+        public string? Diretor { get; set; }
 
-        public string Estrelas { get; set; }
-        public string Estudio { get; set; }
-        public string TituloAlternativo { get; set; }
+        public string? Estrelas { get; set; }
+        public string? Estudio { get; set; }
+        public string? Titulo_Alternativo { get; set; }
 
     }
 }
