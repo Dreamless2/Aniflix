f21704e9f1b27b5fa8898be3be2be7ba1bba0c84
Dreamless2 <tiagonet50@gmail.com>
2025-03-11 21:58:46 -0300

    Update FilmesView.Designer.cs

--- Contracts/FilmesContracts.cs
+++ Contracts/FilmesContracts.cs
@@ -1,13 +1,43 @@
 ﻿using System;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using TMDbLib.Objects.Movies;
 using System.Collections.Generic;
 
 namespace Aniflix.Contracts
 {
     public class FilmesContracts
     {
+        private static readonly HttpClient _httpClient = new()
+        {
+            BaseAddress = new Uri("https://api.themoviedb.org/3/"),
+            DefaultRequestHeaders = { { "Authorization", "Bearer SEU_TOKEN_AQUI" } }
+        };
 
+        public async Task<T?> GetFromTmdbAsync<T>(string endpoint)
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(endpoint);
+                response.EnsureSuccessStatusCode();
+                string json = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<T>(json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao buscar {endpoint}: {ex.Message}");
+                return default;
+            }
+        }
+
+        public async Task<Movie?> GetMovieAsync(string movieId)
+            => await GetFromTmdbAsync<Movie>($"movie/{movieId}");
+
+        public async Task<Credits?> GetMovieCreditsAsync(int movieId)
+            => await GetFromTmdbAsync<Credits>($"movie/{movieId}/credits");
+
+        public async Task<AlternativeTitles?> GetMovieAlternativeTitlesAsync(int movieId)
+            => await GetFromTmdbAsync<AlternativeTitles>($"movie/{movieId}/alternative_titles");
     }
-}
