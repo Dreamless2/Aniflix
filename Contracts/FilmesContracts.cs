30dc5c2d6cee6c1ca88c1233a2931e2386ea4f30
Dreamless2 <tiagonet50@gmail.com>
2025-03-12 20:35:21 -0300

    Update FilmesContracts.cs

--- Contracts/FilmesContracts.cs
+++ Contracts/FilmesContracts.cs
@@ -8,14 +8,12 @@ namespace Aniflix.Contracts
 {
     public class FilmesContracts
     {
-        private readonly TMDbClient _client = new("SEU_TOKEN_AQUI")
+        private readonly TMDbClient _client = new("d1f15ac0632492b3c3a940032ad6b95b")
         {
             DefaultLanguage = "pt-BR",
             DefaultCountry = "BR",
             Timeout = TimeSpan.FromSeconds(600)
         };
-
-
 
         public async Task<Movie?> GetMovieAsync(string movieId)
             => await _client.GetMovieAsync(int.Parse(movieId), MovieMethods.Credits | MovieMethods.AlternativeTitles);
