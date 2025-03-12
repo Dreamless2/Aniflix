122ddbe2f9b67cc27930d39f3aa0675bf39d0bf8
Dreamless2 <tiagonet50@gmail.com>
2025-03-11 23:04:40 -0300

    Update README.md

--- Models/FilmesModel.cs
+++ Models/FilmesModel.cs
@@ -8,5 +8,18 @@ namespace Aniflix.Models
 {
     public class FilmesModel
     {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Sinopse { get; set; }
+        public string TituloOriginal { get; set; }
+        public string DataLancamento { get; set; }
+        public string Tags { get; set; }
+        public string Genero { get; set; }
+        public string Diretor { get; set; }
+
+        public string Estrelas { get; set; }
+        public string Estudio { get; set; }
+        public string TituloAlternativo { get; set; }
+
     }
 }
