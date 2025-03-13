e7bf638469da63a0b9b7fa80068f13393c6e6031
Dreamless2 <tiagonet50@gmail.com>
2025-03-12 22:26:54 -0300

    Update FilmesServices.cs

--- Services/FilmesServices.cs
+++ Services/FilmesServices.cs
@@ -92,7 +92,7 @@ namespace Aniflix.Services
 
                 if (movie.Credits?.Cast != null)
                 {
-                    estrelasText.Text = string.Join(" ", StringExtensions.RemoveAccents(movie.Credits.Cast.Take(5).Select(person => $"#{person.Name.Replace(" ", "")}")))));
+                    estrelasText.Text = StringExtensions.RemoveAccents(StringExtensions.StripPunctuation(StringExtensions.RemoveDiacritics(string.Join(" ", movie.Credits.Cast.Take(5).Select(person => $"#{person.Name.Replace(" ", "")}")))));
                 }
 
                 if (movie.ProductionCompanies != null)
