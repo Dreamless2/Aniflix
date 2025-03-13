1e9f33f7be9aaa0180cd878c3de8eac75b290f11
Dreamless2 <tiagonet50@gmail.com>
2025-03-13 10:43:24 -0300

    Update BreakOutFilmesModels.cs

--- Models/AnimesModels.cs
+++ Models/AnimesModels.cs
@@ -6,22 +6,22 @@ namespace WatchFlix.Models
     public class AnimesModels
     {
         public int Id { get; set; }
-        public string Codigo { get; set; }
-        public string Titulo { get; set; }
-        public string Audio { get; set; }
-        public string Sinopse { get; set; }
-        public string Titulo_Original { get; set; }
-        public string Data_Lancamento { get; set; }
-        public string Titulo_Alternativo { get; set; }
-        public string Franquia { get; set; }
-        public string Pais_Origem { get; set; }
-        public string Idioma_Original { get; set; }
-        public string Anime { get; set; }
-        public string Autores { get; set; }
-        public string Genero { get; set; }
-        public string Tags { get; set; }
-        public string Diretor { get; set; }
-        public string Estrelas { get; set; }
-        public string Estudio { get; set; }
+        public string? Codigo { get; set; }
+        public string? Titulo { get; set; }
+        public string? Audio { get; set; }
+        public string? Sinopse { get; set; }
+        public string? Titulo_Original { get; set; }
+        public string? Data_Lancamento { get; set; }
+        public string? Titulo_Alternativo { get; set; }
+        public string? Franquia { get; set; }
+        public string? Pais_Origem { get; set; }
+        public string? Idioma_Original { get; set; }
+        public string? Anime { get; set; }
+        public string? Autores { get; set; }
+        public string? Genero { get; set; }
+        public string? Tags { get; set; }
+        public string? Diretor { get; set; }
+        public string? Estrelas { get; set; }
+        public string? Estudio { get; set; }
     }
 }
