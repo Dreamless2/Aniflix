03b180568e9874143bc65601ed63b8e0f07211fc
Dreamless2 <tiagonet50@gmail.com>
2025-03-12 22:12:44 -0300

    Update FilmesEntities.cs

--- Entities/FilmesEntities.cs
+++ Entities/FilmesEntities.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.Generic;
-
-namespace Aniflix.Entities
+﻿namespace Aniflix.Entities
 {
     public class FilmesEntities(string titulo, string franquia, string sinopse = null!, string tituloOriginal = null!,
                string dataLancamento = null!, string tituloAlternativo = null!, string filme = null!, string genero = null!, string tags = null!,
