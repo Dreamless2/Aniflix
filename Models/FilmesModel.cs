d821c5dfd11d8754ec49aa5b40fb3711d82c2529
Dreamless2 <tiagonet50@gmail.com>
2025-03-12 22:02:12 -0300

    Update FilmesView.Designer.cs

--- Models/FilmesModel.cs
+++ Models/FilmesModel.cs
@@ -3,26 +3,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Aniflix.Models
 {
-    public class FilmesModel
+    [Table("filmes")]
+    public class FilmesModels
     {
         public int Id { get; set; }
-        public string? Codigo { get; set; }
-        public string? Titulo { get; set; }
-        public string? Audio { get; set; }
-        public string? Sinopse { get; set; }
-        public string? Titulo_Original { get; set; }
-        public string? Data_Lancamento { get; set; }
-        public string? Titulo_Alternativo { get; set; }
-        public string? Filme { get; set; }
-        public string? Franquia { get; set; }
-        public string? Genero { get; set; }
-        public string? Tags { get; set; }
-        public string? Diretor { get; set; }
-        public string? Estrelas { get; set; }
-        public string? Estudio { get; set; }
-        public string? MCU { get; set; }
+        public string Codigo { get; set; }
+        public string Titulo { get; set; }
+        public string Audio { get; set; }
+        public string Sinopse { get; set; }
+        public string Titulo_Original { get; set; }
+        public string Data_Lancamento { get; set; }
+        public string Titulo_Alternativo { get; set; }
+        public string Filme { get; set; }
+        public string Franquia { get; set; }
+        public string Genero { get; set; }
+        public string Tags { get; set; }
+        public string Diretor { get; set; }
+        public string MCU { get; set; }
+        public string Estrelas { get; set; }
+        public string Estudio { get; set; }
     }
 }
