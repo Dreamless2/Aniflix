23b60b4b904debc38d37e8d3b64b9dcca7943adb
Dreamless2 <tiagonet50@gmail.com>
2025-03-12 21:47:58 -0300

    Update GlobalVars.cs

--- Functions/GlobalVars.cs
+++ Functions/GlobalVars.cs
@@ -11,6 +11,7 @@ namespace Aniflix.Functions
         public static Random? random;
 
         public static string TMDB_KEY = "d1f15ac0632492b3c3a940032ad6b95b";
+        public static string DEEPL_KEY = "7feb3eb8-de95-4312-843c-1064aecdab8b:fx";
 
     }
 }
