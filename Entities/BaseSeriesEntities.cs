342d66895be1af6f32e03d04850088e5002422af
Dreamless2 <tiagonet50@gmail.com>
2025-03-13 10:49:58 -0300

    Update BaseAnimesEntities.cs

--- Entities/BaseSeriesEntities.cs
+++ Entities/BaseSeriesEntities.cs
@@ -1,6 +1,6 @@
 ﻿namespace WatchFlix.Services
 {
-    public class BaseSeriesServices(string titulo, string audio, string sinopse = null, string tituloOriginal = null,
+    public class BaseSeriesEntities(string titulo, string audio, string sinopse = null, string tituloOriginal = null,
                string dataLancamento = null, string tituloAlternativo = null, string paisOrigem = null, string idiomaOriginal = null, string serie = null,
                string autores = null, string criadores = null, string obraOriginal = null, string genero = null, string tags = null, string diretor = null,
                string mcu = null, string estrelas = null, string estudio = null)
