78b898b7ee1135532c1dd3265706b3c7a6c02e59
Dreamless2 <tiagonet50@gmail.com>
2025-03-12 22:11:58 -0300

    Update Aniflix.csproj

--- Entities/FilmesEntities.cs
+++ Entities/FilmesEntities.cs
@@ -1,12 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Aniflix.Entities
 {
-    class FilmesEntities
+    public class FilmesEntities(       string titulo, string franquia, string sinopse = null, string tituloOriginal = null,
+               string dataLancamento = null, string tituloAlternativo = null, string filme = null, string genero = null, string tags = null,
+               string diretor = null, string mcu = null, string estrelas = null, string estudio = null, string audio = null)
     {
+        public string Titulo { get; private set; } = titulo;
+        public string Audio { get; private set; } = audio;
+        public string Sinopse { get; private set; } = sinopse;
+        public string TituloOriginal { get; private set; } = tituloOriginal;
+        public string DataLancamento { get; private set; } = dataLancamento;
+        public string TituloAlternativo { get; private set; } = tituloAlternativo;
+        public string Filme { get; private set; } = filme;
+        public string Franquia { get; private set; } = franquia;
+        public string Genero { get; private set; } = genero;
+        public string Tags { get; private set; } = tags;
+        public string Diretor { get; private set; } = diretor;
+        public string MCU { get; private set; } = mcu;
+        public string Estrelas { get; private set; } = estrelas;
+        public string Estudio { get; private set; } = estudio;
+
+        public string GetFormattedText()
+        {
+            string formattedText =
+$@"**{Titulo}** - **{Audio}**
+
+**HD** - __720p__
+**SD** - __480p__
+__[Os vídeos estão em ordem crescente, ou seja, de cima para baixo, tal como na descrição das resoluções.]__
+
+**Sinopse:** __{Sinopse}__
+
+**Título Original:** __{TituloOriginal}__
+**Título Alternativo:** __{TituloAlternativo}__
+**Data de lançamento:** __{DataLancamento}__
+**Filme:** {Filme}
+**Franquia:** {Franquia}
+**Gênero:** {Genero}
+**Tags:** {Tags}
+**Diretor:** {Diretor}
+**Fase MCU:** {MCU}
+**Estrelas:** {Estrelas}
+**Estúdio:** {Estudio}
+";
+            return formattedText;
+        }
     }
 }
+
