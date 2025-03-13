a18657fa41055a4d9680f3ba7d5a42ad50d9fd5f
Dreamless2 <tiagonet50@gmail.com>
2025-03-13 20:21:36 -0300

    Update TrashflixView.Designer.cs

--- Views/TrashflixView.Designer.cs
+++ Views/TrashflixView.Designer.cs
@@ -179,6 +179,7 @@
             SinopseText.Style = Sunny.UI.UIStyle.Custom;
             SinopseText.TabIndex = 83;
             SinopseText.TextAlignment = ContentAlignment.TopLeft;
+            SinopseText.TextChanged += SinopseText_TextChanged;
             // 
             // ResumoText
             // 
