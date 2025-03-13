6314feb4dcd594be5de090efe2ae555db21b83e8
Dreamless2 <tiagonet50@gmail.com>
2025-03-12 22:55:09 -0300

    Update Aniflix.csproj FilmesPresenter.cs

--- Presenters/FilmesPresenter.cs
+++ Presenters/FilmesPresenter.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Aniflix.Functions;
+using Aniflix.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,100 @@ namespace Aniflix.Presenters
 {
     class FilmesPresenter
     {
+    }public static void Registrar(FilmesModels item)
+        {
+            var geral = GlobalVars.context.Filmes
+                .Where(x => x.Codigo == item.Codigo)
+                .FirstOrDefault();
+
+            if (geral == null)
+            {
+                GlobalVars.context.Filmes.Add(item);
+                GlobalVars.context.SaveChanges();
+                MessageBox.Show(item.Titulo + " cadastrado com sucesso!", "Filmes", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show(item.Titulo + " já está cadastrado!", "Filmes", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        public static void Atualizar(FilmesModels item)
+        {
+            var itemExistente = GlobalVars.context.Filmes
+                .Where(x => x.Codigo == item.Codigo)
+                .SingleOrDefault();
+
+            if (itemExistente != null)
+            {
+                itemExistente.Titulo = item.Titulo;
+                itemExistente.Audio = item.Audio;
+                itemExistente.Sinopse = item.Sinopse;
+                itemExistente.Titulo_Original = item.Titulo_Original;
+                itemExistente.Data_Lancamento = item.Data_Lancamento;
+                itemExistente.Titulo_Alternativo = item.Titulo_Alternativo;
+                itemExistente.Filme = item.Filme;
+                itemExistente.Franquia = item.Franquia;
+                itemExistente.Genero = item.Genero;
+                itemExistente.Tags = item.Tags;
+                itemExistente.Diretor = item.Diretor;
+                itemExistente.MCU = item.MCU;
+                itemExistente.Estrelas = item.Estrelas;
+                itemExistente.Estudio = item.Estudio;
+                GlobalVars.context.SaveChanges();
+                MessageBox.Show(item.Titulo + " atualizado com sucesso!", "Filmes", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show(item.Titulo + " não encontrado nos registros!", "Filmes", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        public static FilmesModels GetFirstRow()
+        {
+            return GlobalVars.context.Filmes
+                .OrderBy(x => x.Id)
+                .FirstOrDefault();
+        }
+
+        public static FilmesModels GetNearRow(int id)
+        {
+            try
+            {
+                return GlobalVars.context.Filmes
+                    .Where(x => x.Id > id)
+                    .OrderBy(x => x.Id)
+                    .FirstOrDefault();
+
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static FilmesModels GetPriorRow(int id)
+        {
+            try
+            {
+                if (id == 0)
+                {
+                    return null;
+                }
+
+                return GlobalVars.context.Filmes
+                    .Where(x => x.Id < id)
+                    .OrderByDescending(x => x.Id)
+                    .FirstOrDefault();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static bool Existe(int id)
+        {
+            return GlobalVars.context.Filmes.Any(x => x.Id == id);
+        }
     }
-}
