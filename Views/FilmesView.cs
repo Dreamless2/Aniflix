a4980b7549f7f6af81220167589d66e0da31d6e8
Dreamless2 <tiagonet50@gmail.com>
2025-03-13 10:06:30 -0300

    Update FilmesView.cs

--- Views/FilmesView.cs
+++ Views/FilmesView.cs
@@ -55,7 +55,7 @@ namespace Aniflix.Views
             var services = new FilmesServices();
             if (!int.TryParse(CodigoText.Text, out var codigo) || codigo <= 0)
             {
-                UIMessageBox.ShowError("Informe um código de filme válido.");
+                UIMessageBox.ShowError("Informe um código de filme válido.", false, 1000);
                 CodigoText.Focus();
             }
             else
