eef7032bc07160a4a02b2c6c36370b04af8132d2
Dreamless2 <tiagonet50@gmail.com>
2025-03-12 23:03:08 -0300

    Update Aniflix.csproj

--- Data/GeneralContext.cs
+++ Data/GeneralContext.cs
@@ -1,24 +1,17 @@
-﻿using Aniflix.Models;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Linq;
 using System.Text;
+using Aniflix.Models;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
 namespace Aniflix.Data
 {
-    public class GeneralContext : DBContext
+    public class GeneralContext : DbContext
     {
         private static readonly string localConnectionString = "Server=localhost;Database=aniflix;Username=Covenant9687;Password=v*##GLBkB3r9tuUt;Port=3306;";
         public DbSet<FilmesModels> Filmes { get; set; }
-        public DbSet<GoreFilmesModels> Gore_Filmes { get; set; }
-        public DbSet<BreakOutFilmesModels> Breakout_Filmes { get; set; }
-        public DbSet<SeriesModels> Series { get; set; }
-        public DbSet<GoreSeriesModels> Gore_Series { get; set; }
-        public DbSet<BreakOutSeriesModels> BreakOut_Series { get; set; }
-        public DbSet<AnimesModels> Animes { get; set; }
-        public DbSet<AnimesFilmesModels> Animes_Filmes { get; set; }
-        public DbSet<TrashflixModels> Trashflix { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
