6f31cfc2424d30843ccbe15365e381e64f653ba6
Dreamless2 <tiagonet50@gmail.com>
2025-03-12 22:16:07 -0300

    Update FilmesView.cs

--- Views/FilmesView.cs
+++ Views/FilmesView.cs
@@ -74,5 +74,66 @@ namespace Aniflix.Views
         {
             ChangeData();
         }
+
+        private void SinopseText_EditValueChanged(object sender, EventArgs e)
+        {
+            ChangeData();
+        }
+
+        private void TituloOriginalText_EditValueChanged(object sender, EventArgs e)
+        {
+            ChangeData();
+        }
+
+        private void DataLancamentoText_EditValueChanged(object sender, EventArgs e)
+        {
+            ChangeData();
+        }
+
+        private void TituloAlternativoText_EditValueChanged(object sender, EventArgs e)
+        {
+            ChangeData();
+        }
+
+        private void TagsText_EditValueChanged(object sender, EventArgs e)
+        {
+            ChangeData();
+        }
+
+        private void FilmeText_EditValueChanged(object sender, EventArgs e)
+        {
+            ChangeData();
+        }
+
+        private void FranquiaText_EditValueChanged(object sender, EventArgs e)
+        {
+            ChangeData();
+        }
+
+        private void GeneroText_EditValueChanged(object sender, EventArgs e)
+        {
+            ChangeData();
+        }
+
+        private void DiretorText_EditValueChanged(object sender, EventArgs e)
+        {
+            ChangeData();
+        }
+
+        private void EstrelasText_EditValueChanged(object sender, EventArgs e)
+        {
+            ChangeData();
+        }
+
+        private void EstudioText_EditValueChanged(object sender, EventArgs e)
+        {
+            ChangeData();
+        }
+
+        private void FaseMCUText_EditValueChanged(object sender, EventArgs e)
+        {
+            ChangeData();
+        }
+
     }
 }
