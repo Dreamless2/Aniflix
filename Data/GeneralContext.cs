28778786bc249c39533347aa505bbe9d323466ce
Dreamless2 <tiagonet50@gmail.com>
2025-03-13 13:47:09 -0300

    Update FilmesContracts.cs

--- Data/GeneralContext.cs
+++ Data/GeneralContext.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Text;
-using Aniflix.Models;
-using System.Threading.Tasks;
-using System.Collections.Generic;
+﻿using Aniflix.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Aniflix.Data
