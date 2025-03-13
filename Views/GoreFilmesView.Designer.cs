dd344fbbe698bf1df41b74eb3f2a72aba0121a86
Dreamless2 <tiagonet50@gmail.com>
2025-03-13 20:16:37 -0300

    Update GoreFilmesView.Designer.cs

--- Views/GoreFilmesView.Designer.cs
+++ Views/GoreFilmesView.Designer.cs
@@ -92,6 +92,7 @@
             ProximoButton.Text = "Proximo";
             ProximoButton.TextAlign = ContentAlignment.BottomLeft;
             ProximoButton.UseVisualStyleBackColor = false;
+            ProximoButton.Click += ProximoButton_Click;
             // 
             // AnteriorButton
             // 
