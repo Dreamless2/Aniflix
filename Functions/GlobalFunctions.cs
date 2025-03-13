ff2499fb631d9381e951fbcab273ca9c4ab62caa
Dreamless2 <tiagonet50@gmail.com>
2025-03-12 23:04:41 -0300

    Update GeneralContext.cs

--- Functions/GlobalFunctions.cs
+++ Functions/GlobalFunctions.cs
@@ -145,5 +145,25 @@ namespace Aniflix.Functions
                 throw new Exception($"Erro ao carregar o arquivo SVG: {ex.Message}");
             }
         }
+
+        public static void UndoReadOnly(Control control)
+        {
+            foreach (Control c in control.Controls)
+            {
+                if (c.Controls != null && c.Controls.Count > 0)
+                {
+                    UndoReadOnly(c);
+                }
+                else if (c is UITextBox box)
+                {
+                    box.ReadOnly = false;
+                }
+                else if (c is UIComboBox comboBox)
+                {
+                    comboBox.Enabled = true;
+                }
+            }
+        }
+
     }
 }
