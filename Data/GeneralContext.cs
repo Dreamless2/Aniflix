c7a478d4dcbaab86622650dcbc5169f2d1fd8bd3
Dreamless2 <tiagonet50@gmail.com>
2025-03-13 10:46:23 -0300

    Update BreakOutSeriesPresenter.cs

--- Data/GeneralContext.cs
+++ Data/GeneralContext.cs
@@ -12,6 +12,15 @@ namespace Aniflix.Data
     {
         private static readonly string localConnectionString = "Server=localhost;Database=aniflix;Username=Covenant9687;Password=v*##GLBkB3r9tuUt;Port=3306;";
         public DbSet<FilmesModels> Filmes { get; set; }
+        public DbSet<GoreFilmesModels> Gore_Filmes { get; set; }
+        public DbSet<BreakOutFilmesModels> Breakout_Filmes { get; set; }
+        public DbSet<SeriesModels> Series { get; set; }
+        public DbSet<GoreSeriesModels> Gore_Series { get; set; }
+        public DbSet<BreakOutSeriesModels> BreakOut_Series { get; set; }
+        public DbSet<AnimesModels> Animes { get; set; }
+        public DbSet<AnimesFilmesModels> Animes_Filmes { get; set; }
+        public DbSet<TrashflixModels> Trashflix { get; set; }
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
