18232deeb13b0bc2fd7fb49549f6971ad3f9617d
Dreamless2 <tiagonet50@gmail.com>
2025-03-13 20:22:32 -0300

    Update TrashflixView.Designer.cs

--- Views/TrashflixView.Designer.cs
+++ Views/TrashflixView.Designer.cs
@@ -254,6 +254,7 @@
             EstrelasText.TextAlignment = ContentAlignment.MiddleLeft;
             EstrelasText.Watermark = "Estrelas";
             EstrelasText.WordWarp = false;
+            EstrelasText.TextChanged += EstrelasText_TextChanged;
             // 
             // EstudioText
             // 
