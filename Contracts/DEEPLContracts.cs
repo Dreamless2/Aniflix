a1432a275538f92f172114356ce95dad00a28afd
Dreamless2 <tiagonet50@gmail.com>
2025-03-14 11:34:28 -0300

    Update DEEPLContracts.cs

--- Contracts/DEEPLContracts.cs
+++ Contracts/DEEPLContracts.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using TMDbLib.Client;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Aniflix.Contracts
 {
     public class DEEPLContracts
     {
-        private readonly TMDbClient _client = new("d1f15ac0632492b3c3a940032ad6b95b")
+        private readonly DeepLClient _client = new("d1f15ac0632492b3c3a940032ad6b95b")
         {
             DefaultLanguage = "pt-BR",
             DefaultCountry = "BR",
