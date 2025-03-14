4e5a9abc58f2a1dd3a415b4029318a560a8806c7
Dreamless2 <tiagonet50@gmail.com>
2025-03-14 10:50:41 -0300

    Update GeneralFilmesContracts.cs

--- Contracts/GeneralFilmesContracts.cs
+++ Contracts/GeneralFilmesContracts.cs
@@ -16,7 +16,7 @@ namespace Aniflix.Contracts
         public async Task<Movie?> GetMovieAsync(string movieId)
             => await _client.GetMovieAsync(int.Parse(movieId), MovieMethods.Credits | MovieMethods.AlternativeTitles);
 
-        public async Task<TvShow?> SearchMovieAsync(string showID)
+        public async Task<TvShow?> GetTvShowAsync(string showID)
             => await _client.GetTvShowAsync(int.Parse(showID), TvShowMethods.Credits | TvShowMethods.AlternativeTitles);
     }
 }
