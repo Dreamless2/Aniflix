9aaa89a2588bc26f4aac60f60ddaab54265c1ed5
Dreamless2 <tiagonet50@gmail.com>
2025-03-14 11:39:45 -0300

    Update DEEPLContracts.cs

--- Contracts/DEEPLContracts.cs
+++ Contracts/DEEPLContracts.cs
@@ -6,16 +6,7 @@ namespace Aniflix.Contracts
     public class DEEPLContracts
     {
         private readonly DeepLClient _client = new(GlobalVars.DEEPL_KEY);
-
         private readonly string _sourceLanguage = "EN";
         private readonly string _targetLanguage = "PT-BR";
-
-        public DEEPLContracts()
-        {
-
-        }
-
-
-
     }
 }
