ca08ccf99b032381989cece395056080312f0c9a
Dreamless2 <tiagonet50@gmail.com>
2025-03-14 11:48:22 -0300

    Update DEEPLContracts.cs

--- Contracts/DEEPLContracts.cs
+++ Contracts/DEEPLContracts.cs
@@ -6,16 +6,7 @@ namespace Aniflix.Contracts
 {
     public class DEEPLContracts
     {
-
-        var DeepLClientOptions _options = new ()
-        {
-            MaximumNetworkRetries = 5,
-            PerRetryConnectionTimeout = TimeSpan.FromSeconds(10),
-            sendPlatformInfo = false,
-        };
-
-
-    DeepLClient _client = new(GlobalVars.DEEPL_KEY, _options)
+        private readonly DeepLClient _client = new(GlobalVars.DEEPL_KEY);
 
     }
 
