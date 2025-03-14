bcd075c9c97a4d139fce44302efdabbf9c28a359
Dreamless2 <tiagonet50@gmail.com>
2025-03-13 22:57:03 -0300

    Update BreakOutFilmesPresenter.cs

--- Views/BreakOutFilmesView.cs
+++ Views/BreakOutFilmesView.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using Sunny.UI;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Drawing;
+﻿using Sunny.UI;
 using Aniflix.Models;
 using Aniflix.Entities;
 using Aniflix.Services;
 using Aniflix.Functions;
 using Aniflix.Presenters;
-using System.Windows.Forms;
-using System.ComponentModel;
-using System.Threading.Tasks;
-using System.Collections.Generic;
 
 namespace Aniflix.Views
 {
