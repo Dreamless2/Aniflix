973d750f33b69d007c648bc13fa54c1b1e9eaff8
Dreamless2 <tiagonet50@gmail.com>
2025-03-13 22:20:26 -0300

    Update BreakOutFilmesView.cs

--- Views/BreakOutFilmesView.cs
+++ Views/BreakOutFilmesView.cs
@@ -102,7 +102,7 @@ namespace Aniflix.Views
         #region "Leave"
         private async void CodigoText_Leave(object sender, EventArgs e)
         {
-            var services = new GoreFilmesServices();
+            var services = new BreakOutFilmesServices();
             if (!int.TryParse(CodigoText.Text, out var codigo) || codigo <= 0)
             {
                 UIMessageBox.ShowError("Informe um código válido.", false, 1000);
