43ad89f505ac52a44c48f0925d3cf98ac13b9c3e
Dreamless2 <tiagonet50@gmail.com>
2025-03-15 22:50:07 -0300

    Update SeriesView.Designer.cs

--- Extensions/StringExtensions.cs
+++ Extensions/StringExtensions.cs
@@ -125,7 +125,9 @@ namespace Aniflix.Extensions
         {
             string normalized = input.Normalize(NormalizationForm.FormD);
 
-            string withoutAccents = Regex.Replace(normalized, @"[^\p{L}# ]", "");
+            //string withoutAccents = Regex.Replace(normalized, @"[^\p{L}# ]", "");
+            string withoutAccents = Regex.Replace(normalized, "[^a-zA-Z# ]", "");
+
 
             return Regex.Replace(withoutAccents, @"[\p{P}&&[^#]]", "");
         }
