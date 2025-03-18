6cda6e45fc60e3c51f188d791c648e2fc851998a
Dreamless2 <tiagonet50@gmail.com>
2025-03-18 18:40:50 -0300

    Update GoreFilmesView.cs

--- Views/BreakOutSeriesView.cs
+++ Views/BreakOutSeriesView.cs
@@ -120,6 +120,11 @@ namespace Aniflix.Views
         #region "Leave"
         private async void CodigoText_Leave(object sender, EventArgs e)
         {
+            if (GlobalVars.isClosing)
+            {
+                return;
+            }
+
             if (string.IsNullOrEmpty(CodigoText.Text))
             {
                 MessageBox.Show("Por favor, insira o código da série.", "BreakOut - Séries", MessageBoxButtons.OK, MessageBoxIcon.Error);
