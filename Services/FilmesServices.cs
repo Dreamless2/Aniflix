dc3073ee678882e842af50ab5186aaa882a3fa0a
Dreamless2 <tiagonet50@gmail.com>
2025-07-27 13:18:36 -0300

    AnimesServices.cs

--- Services/FilmesServices.cs
+++ Services/FilmesServices.cs
@@ -63,8 +63,8 @@ namespace Aniflix.Services
                     bool temAcentos = StringExtensions.HasAccents(item.Title);
 
                     filmeText.Text = temAcentos
-                        ? "#" + itemSemAcentos + " " + "#" + itemSemAcentos
-                        : "#" + itemComAcentos;
+                        ? "#" + itemComAcentos + " " + "#" + itemSemAcentos
+                        : "#" + itemSemAcentos;
                 }
                 else
                 {
